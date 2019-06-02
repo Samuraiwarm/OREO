1b9d3998676aa73bee88a0f05e7b80aee237c377
Samuraiwarm <warm.pianist@gmail.com>
2019-06-02 20:50:35 +0700

    ☆ M U L T I OREO TYPES ! ! ! 1 ! ☆
    Todo
    - Generated oreo images
    - Hopefully random reviews, concerning O:RE ratio

--- multiOreo.py
+++ multiOreo.py
@@ -8,10 +8,12 @@ def multiOreo():
 	max = 0
 	max_oreo = ""
 	for i in range(int(n)):
-		size, output = oreo()
+		oreo_count, output = oreo()
+		size = sum(list(oreo_count.values()))
 		if size > max:
-			max, max_oreo = (size, output)
-	print('\nLongest oreo =', max_oreo, 'length = ', max)
+			max, max_oreo, max_oreo_count = (size, output, oreo_count)
+	print('\nLongest oreo =', max_oreo, ', length =', max)
+	print('oreo count:', max_oreo_count) # only the latest max is displayed
 
 # main
 def main():
--- oreo.py
+++ oreo.py
@@ -1,29 +1,41 @@
 import random
 
 # init
-oreo_terminate = 0.1
-oreo_o = oreo_re = (1-oreo_terminate)/2
+oreo_prob = {
+	'O': 180,
+	'RE': 45,
+	'CRE': 30,
+	'SRE': 20,
+}
+oreo_terminate = 0.1 # probability of terminating oreo sequence
+
+# utilities
+oreo_items = list(oreo_prob.items())
 
 # function 
 def oreo():
 	oreo = ""
-	count = 0
+	oreo_count = {key:0 for key in oreo_prob.keys()}
 	while True:
-		rand = random.random()
-		if rand < oreo_terminate:
+		rand_terminate = random.random()
+		if rand_terminate < oreo_terminate:
 			if oreo == "":
 				oreo = "- v o i d -"
 			print(oreo)
-			return count, oreo
-		elif rand < (oreo_terminate + oreo_o):
-			oreo += "O"
-		else:
-			oreo += "RE"
-		count += 1
+			return oreo_count, oreo
+		rand_choice = random.uniform(0, sum(count for name, count in oreo_items))
+		counter = 0
+		for name, count in oreo_items:
+			counter += count
+			if counter >= rand_choice:
+				oreo += name
+				oreo_count[name] += 1
+				break
 
 # main
 def main():
-	oreo()
+	oreo_count, output = oreo()
+	print(oreo_count)
   
 if __name__== "__main__":
 	main()
